b857f009605b3f5c1ea8ad0b5640f294435232e4
huypt03 <huypham3062k3@gmail.com>
2026-05-13 07:17:24 +0700

    [Feat] Modify chat api to chat websocket

--- chat-service/app/apis/chat_api.py
+++ chat-service/app/apis/chat_api.py
@@ -1,15 +1,26 @@
+import logging
+
 from fastapi import APIRouter, Body
 from fastapi.responses import JSONResponse
+from starlette.websockets import WebSocket, WebSocketDisconnect
+from sympy.codegen import While
 
 from app.requests.chat_request import ChatRequest
 
 chat_router = APIRouter()
 import app.services.chat_service as chat_service
 
-@chat_router.post("/chats")
-async def chat(input: ChatRequest):
-    res = chat_service.chat(input)
-    return JSONResponse(content=res, status_code= res.get("status").get("code"))
+@chat_router.websocket("/chats")
+async def chat(web_socket: WebSocket):
+    await web_socket.accept()
+    try:
+        while True:
+            data =  await web_socket.receive_json()
+            input = ChatRequest(**data)
+            await chat_service.chat(web_socket, input)
+    except WebSocketDisconnect:
+        logging.info("Disconnect websocket")
+
 
 @chat_router.post("/standardization-voice-question")
 async def standardization(input: dict = Body(...)):
--- chat-service/app/services/chat_service.py
+++ chat-service/app/services/chat_service.py
@@ -1,6 +1,8 @@
 import logging
 from pathlib import Path
 
+from fastapi import WebSocket;
+
 from app.requests.chat_request import ChatRequest
 from app.services.query_embedding import query_vector_database_course
 from app.utils.constants import Constant
@@ -8,13 +10,13 @@ from app.utils.open_ai_util import init_open_ai
 
 client = init_open_ai()
 
-#GET ABSOLUTE PATH IN THIS FILE
+# GET ABSOLUTE PATH IN THIS FILE
 FILE_DIR = Path(__file__).absolute()
 
-def chat(input: ChatRequest) -> dict:
+
+async def chat(web_socket: WebSocket, input: ChatRequest) -> dict:
     try:
         courses = query_vector_database_course(input.question)
-        print(courses)
 
         # GET PROMPT
         with open(f"{FILE_DIR.parents[2]}/prompt/course.txt", "r") as file:
@@ -22,30 +24,46 @@ def chat(input: ChatRequest) -> dict:
 
         prompt = template.format(course=courses, question=input.question)
 
-        res = client.responses.create(
+        stream = client.responses.create(
             model=input.model,
-            input=prompt
+            input=prompt,
+            stream=True
         )
 
-        return {
-            "data": res.output_text,
-            "status": {
-                "message": "Success!",
-                "code": 200
-            }
-        }
+        for chunk in stream:
+            if chunk.type == "response.output_text.delta":
+                await web_socket.send_json({
+                    "type": "chunk",
+                    "data": chunk.delta,
+                    "status": {
+                        "message": "Success",
+                        "code": 200
+                    }
+                })
+
+        await web_socket.send_json(
+            {
+                "status": {
+                    "message": "Done!",
+                    "code": 200
+                }
+            })
+
     except Exception as e:
         logging.error(f"[ERROR-CHAT] {e}")
-        return {
-            "status":{
+        await web_socket.send_json({
+            "status": {
                 "message": Constant.API_STATUS.INTERNAL_SERVER_ERROR,
                 "code": Constant.API_STATUS.INTERNAL_SERVER_ERROR_CODE
             }
-        }
+        })
+
 
 """
     standardization voice question (course) by user
 """
+
+
 def standardization_voice_question(input: str):
     try:
         # GET PROMPT
@@ -78,5 +96,3 @@ def standardization_voice_question(input: str):
                 "code": Constant.API_STATUS.INTERNAL_SERVER_ERROR_CODE
             }
         }
-
-
