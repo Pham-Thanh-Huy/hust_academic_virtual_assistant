54e9172373cf9af646fcbc080bd29a0f385a4ad9
huypt03 <huypham3062k3@gmail.com>
2026-07-12 12:06:39 +0700

    update config eureka

---
+++ chat-session-service/build_push.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+IMAGE_NAME="chat-session-service"
+TAG="$1"
+DOCKER_USERNAME="$2"
+DOCKER_PASSWORD="$3"
+
+# Kiểm tra tham số đầu vào
+if [ -z "$TAG" ] || [ -z "$DOCKER_USERNAME" ] || [ -z "$DOCKER_PASSWORD" ]; then
+    echo "Usage: $0 <TAG> <DOCKER_USERNAME> <DOCKER_PASSWORD>"
+    exit 1
+fi
+
+echo "Login Docker Hub..."
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+echo "Building and push image..."
+
+docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    -t "phamthanhhuy/${IMAGE_NAME}:${TAG}" \
+    -f Dockerfile \
+    --push .
+
+echo "Build  image success..."
+echo "Push success, image is phamthanhhuy/${IMAGE_NAME}:${TAG}"
+
+echo "Remove local image..."
+docker image rm -f "phamthanhhuy/${IMAGE_NAME}:${TAG}"
+
+echo "Success!"
+exit 0
