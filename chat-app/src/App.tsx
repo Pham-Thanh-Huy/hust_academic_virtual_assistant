c95af3912f65d8d0c8c330b81433e790659c60b5
huypt03 <huypham3062k3@gmail.com>
2026-07-18 09:33:18 +0700

    change to wss

--- chat-app/src/App.tsx
+++ chat-app/src/App.tsx
@@ -1,34 +1,33 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import './App.css'
-import {Chat} from "./components/home"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Chat } from "./components/home";
 import Login from "./components/login/Login.tsx";
-import {ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-
-    return (
-        <>
-            <ToastContainer
-                position="top-right"
-                autoClose={1700}
-                hideProgressBar
-                newestOnTop={false}
-                closeOnClick={false}
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-            />
-            <BrowserRouter>
-                <Routes>
-                    <Route path={'/'} element={<Chat/>}/>
-                    <Route path={'/chat/:id'} element={<Chat/>}/>
-                    <Route path={'/login'} element={<Login/>}/>
-                </Routes>
-            </BrowserRouter>
-        </>
-    )
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={1700}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Chat />} />
+          <Route path={"/chat/:id"} element={<Chat />} />
+          <Route path={"/login"} element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
--- chat-app/src/layouts/header/Header.tsx
+++ chat-app/src/layouts/header/Header.tsx
@@ -1,47 +1,46 @@
-import {FiBell, FiMenu} from "react-icons/fi";
+import { FiBell, FiMenu } from "react-icons/fi";
 
 export const Header = ({
-                           onToggleSidebar,
-                       }: {
-    onToggleSidebar: () => void;
+  onToggleSidebar,
+}: {
+  onToggleSidebar: () => void;
 }) => {
-    return (
-        <header className="flex items-center justify-between bg-white px-5 py-3 shadow-sm border-b border-gray-100">
-            {/* Toggle Sidebar */}
-            <button
-                onClick={onToggleSidebar}
-                className="group flex h-12 w-12 items-center justify-center rounded-xl border border-gray-200 bg-white text-[#9A001F] shadow-sm transition-all duration-300 hover:-translate-y-0.5 hover:bg-[#9A001F] hover:text-white hover:shadow-lg active:scale-95"
-            >
-                <FiMenu
-                    size={26}
-                    className="transition-transform duration-300 group-hover:rotate-180"
-                />
-            </button>
+  return (
+    <header className="flex items-center justify-between bg-white px-5 py-3 shadow-sm border-b border-gray-100">
+      {/* Toggle Sidebar */}
+      <button
+        onClick={onToggleSidebar}
+        className="group flex h-12 w-12 items-center justify-center rounded-xl border border-gray-200 bg-white text-[#9A001F] shadow-sm transition-all duration-300 hover:-translate-y-0.5 hover:bg-[#9A001F] hover:text-white hover:shadow-lg active:scale-95"
+      >
+        <FiMenu
+          size={26}
+          className="transition-transform duration-300 group-hover:rotate-180"
+        />
+      </button>
 
-            {/* Logo */}
-            <div>
-                <img
-                    src="/hust-logo.svg"
-                    alt="HUST"
-                    className="h-12 w-12 object-contain"
-                />
-            </div>
+      {/* Logo */}
+      <div>
+        <img
+          src="/hust-logo.svg"
+          alt="HUST"
+          className="h-12 w-12 object-contain"
+        />
+      </div>
 
-            {/* Notification */}
-            <button
-                className="relative group flex h-12 w-12 items-center justify-center rounded-xl border border-gray-200 bg-white text-[#9A001F] shadow-sm transition-all duration-300 hover:bg-[#9A001F] hover:text-white hover:shadow-lg">
-                {/* Ping */}
-                <span className="absolute right-2 top-2 flex h-3 w-3">
+      {/* Notification */}
+      <button className="relative group flex h-12 w-12 items-center justify-center rounded-xl border border-gray-200 bg-white text-[#9A001F] shadow-sm transition-all duration-300 hover:bg-[#9A001F] hover:text-white hover:shadow-lg">
+        {/* Ping */}
+        <span className="absolute right-2 top-2 flex h-3 w-3">
           <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-500 opacity-75"></span>
           <span className="relative inline-flex h-3 w-3 rounded-full bg-red-600"></span>
         </span>
 
-                {/* Bell */}
-                <FiBell
-                    size={24}
-                    className="origin-top animate-[wiggle_1.4s_ease-in-out_infinite]"
-                />
-            </button>
-        </header>
-    );
+        {/* Bell */}
+        <FiBell
+          size={24}
+          className="origin-top animate-[wiggle_1.4s_ease-in-out_infinite]"
+        />
+      </button>
+    </header>
+  );
 };
